9a76c58f514f656383fe70df4daa82a803e242e1
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-09 18:20:41 +0400

    relise one

---
+++ SquareRoot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double e = 256,
+            rez = Math.Sqrt(e);
+            Console.WriteLine("kvadratni koren 256 = {0}", rez);
+            Console.ReadKey();
+        }
+    }
+}
